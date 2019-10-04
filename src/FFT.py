3b70be1c790e2079d00c11f5495f618d92950b85
Charlie Tharas <charlietharas@hunterschools.org>
2019-10-04 17:17:01 -0400

    Recommmit changes lost on win-src

--- src/FFT.py
+++ src/FFT.py
@@ -1,51 +1,41 @@
 '''
 Created on Sep 12, 2019
-
 @author: charlie_tharas
 '''
 
-from playsound import playsound
+import os
+import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile
-import matplotlib.pyplot as plt
-import os
-from scipy.signal.signaltools import detrend
-from numpy.lib.function_base import average
-
-print("Plotting successful.")
-
-# Note that filepaths vary off of OS & Location (see filepath list doc)
-def loadFFTGraph(filepath, ):
-    rate, data = wavfile.read("/home/charlie-t/eclipse-ml/fourier/res/cut/allthistime_20ms.wav")
-    fft_out = fft(data)
-    # TODO (see visualize())
-
-
-def playaudio(audiostring):
-    playsound(audiostring)
-
+from numpy.core._multiarray_umath import arange
 
 def visualize():
     # going through fft in cut directory
-    for file in os.listdir("/home/charlie-t/eclipse-ml/fourier/res/cut/"):
+    for file in os.listdir("D:/Coding/GitHub/fourier/res/cut/"):
         filename = os.fsdecode(file)
         print("Check " + filename)
         if filename[len(filename)-3:len(filename)] == "wav":
-            readfilepath = "/home/charlie-t/eclipse-ml/fourier/res/cut/" + filename
-            rate, data = wavfile.read(readfilepath)
-            data = detrend(data, 0)
-            fft_out = fft(data)
-            
-            # skip plotting if data contains nothing
-            if average(data) != 0:
-                plt.plot(fft_out)
-                plt.show(block=False)
-                plt.xlabel("Frequency (Hz)")
-                plt.xlim(500, 1500)
-                plt.pause(1)
-                plt.clf()
+            readfilepath = "D:/Coding/GitHub/fourier/res/cut/" + filename
+            loadFFTGraph(readfilepath)
             
     plt.show()
+
+# Note that filepaths vary off of OS & Location (see filepath list doc)
+def loadFFTGraph(filepath):
+    rate, data = wavfile.read(filepath)
     
-loadFFTGraph("D:/Coding/GitHub/fourier/res/allthistime_20ms.wav")
+    channel = data.T[0] # grab 1st channel
+    normalize = [(i/2**8.)*2-1 for i in channel] # normalize track
+    fft_data = fft(normalize) # perform fourier transform
+    fft_out = len(fft_data)/2 # half of fft (signal symmetry)
+    fft_out = int(fft_out) # normalize integer function for plot
+    
+    a = arange(len(data))
+    b = len(data)/rate
+    frqLabel = a/b # unknown application
+    
+    plt.plot(abs(fft_data[:(fft_out-1)]),'r')
+    plt.show(block=False) # disable for constant visualization
+    plt.pause(1)
+    
 visualize()
